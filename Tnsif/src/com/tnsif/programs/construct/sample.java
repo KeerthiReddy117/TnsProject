82793c94bb0cd1ad2670529eda766ea9d882d2de
Keerthi Reddy <keerthisanjeevareddy@gmail.com>
2026-03-11 17:54:37 +0530

    commit

---
+++ Tnsif/src/com/tnsif/programs/construct/sample.java
@@ -0,0 +1,15 @@
+package com.tnsif.programs.construct;
+
+public class sample {
+	
+	int stdid;
+	String stdname;
+	int smarks;
+	
+	sample()
+	{
+		System.out.println("This is default con");
+	}
+
+	
+}
