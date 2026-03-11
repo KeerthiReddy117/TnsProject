b414164923c2ee1e29095c928965bb86eb70f85a
Keerthi Reddy <keerthisanjjevareddy@gmail.com>
2026-03-11 13:39:15 +0530

    third programs

---
+++ Tnsif/src/com/tnsif/packages/basedemo.java
@@ -0,0 +1,13 @@
+package com.tnsif.packages;
+
+public class basedemo {
+
+	public static void main(String[] args) {
+		base ob = new base();
+		ob.setEid(101);
+		ob.setEname("Hello");
+		System.out.println(ob.getEid());
+		System.out.println(ob.getEname());
+	}
+
+}
